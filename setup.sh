37161e88225ab597a9a76b7049b935c471cd5509
wlonestar <wlonestar@163.com>
2023-06-10 19:24:04 +0800

    add setup shell

---
+++ setup.sh
@@ -0,0 +1,7 @@
+
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+
+fisher install jethrokuan/z
+fisher install pure-fish/pure
+
+
