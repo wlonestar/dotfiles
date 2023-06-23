e5ac98453fceca1dec224d98762309d75dd69a45
wlonestar <wlonestar@163.com>
2023-06-23 11:01:27 +0800

    update configt

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# opam configuration
+source /home/wjl/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
